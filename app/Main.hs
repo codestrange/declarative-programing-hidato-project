0dd0bc39c362131df947249de69ee9beb1bf4fc0
leynier <leynier41@gmail.com>
2020-10-22 11:49:03 -0400

    Add gen to solve

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,16 @@
 module Main where
 
+import System.Random
+
 import Structures
 import Algorithms
 
 
 main = do 
+        let rand = randomIO :: IO Int
+        seed <- rand
+        let gen = mkStdGen seed
+
         let m = generate
-        let sol = solve m
+        let sol = solve m gen
         print sol
